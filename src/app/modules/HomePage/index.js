8ddc603dfeaeaad8bdbaee5017b917d34d4155d4
nishant <nishant.singh87@gmail.com>
2019-04-01 05:01:35 +0530

    Adding scroll listener with throttle.

--- src/app/modules/HomePage/index.js
+++ src/app/modules/HomePage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Splash from './Splash';
+import ScrollListener from '../../shared/ScrollListener';
 
 const scrolledReached = selector => document.querySelector(selector).getClientRects()[0].y < 1;
 
@@ -13,20 +14,21 @@ class HomePage extends React.PureComponent{
   };
 
   componentDidMount(){
-    window.addEventListener('scroll', ()=> {
+    const onWindowScroll = ()=> {
       const homepageReached = scrolledReached("#homepage .homepage-content");
       let splashScrollState = 'four-fill';
 
       if(!homepageReached) {
-          splashScrollState =
-            scrolledReached("#homepage ul.splash-scroll-monitor > li:nth-child(4)") ? 'four-fill' :
+        splashScrollState =
+          scrolledReached("#homepage ul.splash-scroll-monitor > li:nth-child(4)") ? 'four-fill' :
             scrolledReached("#homepage ul.splash-scroll-monitor > li:nth-child(3)") ? 'three-expand':
-            scrolledReached("#homepage ul.splash-scroll-monitor > li:nth-child(2)") ? 'two-begin'  :
-            'one-sketch';
+              scrolledReached("#homepage ul.splash-scroll-monitor > li:nth-child(2)") ? 'two-begin'  :
+                'one-sketch';
       }
 
       this.setState({homepageReached, splashScrollState})
-    });
+    };
+    this.scrollListener = ScrollListener.$window(onWindowScroll);
   }
 
   render(){
---
+++ src/app/shared/ScrollListener.js
@@ -0,0 +1,30 @@
+const SCROLL_PROCESS_DELAY_MS = 100;
+
+let listeners = [];
+
+const processScrollEvent = (event) => {
+  console.log({event});
+  listeners.forEach(l=> l())
+};
+
+const createWindowListener = newListener => {
+  listeners.push(newListener);
+  return {destroy: () => {listeners = listeners.filter(l => l === newListener)}};
+};
+
+const ScrollListener = {
+  $window: createWindowListener
+};
+
+const throttled = callback => {
+  let nextCall;
+
+  return event => {
+    if(nextCall) clearTimeout(nextCall);
+    nextCall = setTimeout(() => callback(event), SCROLL_PROCESS_DELAY_MS);
+  };
+};
+
+window.addEventListener('scroll', throttled(processScrollEvent));
+
+export default ScrollListener;
