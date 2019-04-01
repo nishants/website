a5b5b72103f80363814e44cd0852b257846ecdac
nishant <nishant.singh87@gmail.com>
2019-04-01 20:13:10 +0530

    Create portfolio page.

--- src/app/modules/HomePage/Portfolio.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Portfolio = () => <h1>Portfolio</h1>;
-
-export default Portfolio;
---
+++ src/app/modules/HomePage/Portfolio/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Portfolio extends React.PureComponent {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div id="portfolio-page">
+        <h1>Portfolio</h1>
+      </div>
+    );
+  }
+}
+export default Portfolio;
