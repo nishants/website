5e06cf536a1d4a5a3459ed8cf2245944061557ac
nishant <nishant.singh87@gmail.com>
2019-04-01 21:58:52 +0530

    Addig social links and prev links in sidebar.

--- src/app/modules/HomePage/SideBar/index.js
+++ src/app/modules/HomePage/SideBar/index.js
@@ -23,6 +23,63 @@ const SieBar = () => (
         <Link to="/portfolio/xyz">Continuous Integration</Link>
       </li>
     </ul>
+
+    <label>Previous</label>
+    <ul className="previous-links">
+      <li>
+        <Link to="/work-history/xyz">ThoughtWorks</Link>
+      </li>
+      <li>
+        <Link to="/work-history/xyz"> Oracle</Link>
+      </li>
+      <li>
+        <Link to="/work-history/xyz"> Infosys</Link>
+      </li>
+    </ul>
+
+    <label>Connect</label>
+    <ul className="social-links">
+      <li className="github">
+        {' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/nishants"
+        >
+          <i className="fab fa-github" />Github
+        </a>
+      </li>
+      <li className="stack-overflow">
+        {' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://stackoverflow.com/users/1065020/dawn"
+        >
+          <i className="fab fa-stack-overflow" />StackOveflow
+        </a>
+      </li>
+      <li className="twitter">
+        {' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/nshnt_sng"
+        >
+          <i className="fab fa-twitter" />Twitter
+        </a>
+      </li>
+      <li className="linkedin">
+        {' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/nishant-singh-b2420946"
+        >
+          <i className="fab fa-linkedin" />Linkedin
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
